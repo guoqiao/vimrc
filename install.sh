e2dbadf2de152e29e3a81260cf64c80f5eeeb6a1
guoqiao <guoqiao@gmail.com>
2014-12-14 19:56:16 +1300

    simple install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,4 @@
 mkdir -p bundle
 git clone https://github.com/gmarik/vundle.git bundle/vundle
 ln -is ~/.vim/vimrc ~/.vimrc
-ln -is ~/.vim/vimrc ~/.gvimrc
-ln -is ~/.vim/vimrc ~/.nvimrc
-ln -is ~/.nvim ~/.vim
-sudo pip install autopep8
 vim +BundleInstall +qall
