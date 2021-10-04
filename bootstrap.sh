ff2d67854deb5ad97a694ce7f5e099340fe75059
Joe Guo <joe.guo@canonical.com>
2021-10-04 23:20:22 +1300

    bootstrap.sh: rm font stuff
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,5 @@
 
 sudo apt update
 sudo apt upgrade --yes
-sudo apt install --yes exuberant-ctags fontconfig
+sudo apt install --yes exuberant-ctags
 sudo python3 -m pip install -U jedi black
-# for user only: ~/.local/share/fonts/
-sudo cp -rfuv fonts/* /usr/local/share/fonts/
-# list all fonts: fc-list
-sudo fc-cache -fv
