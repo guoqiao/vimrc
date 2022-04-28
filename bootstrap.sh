6a0541c6e3e7a602b670ed70eee34d5ced4eab61
joegraviton <joe@graviton.xyz>
2022-04-29 08:32:42 +1200

    update bootstrap.sh to support both brew and apt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo apt update
-sudo apt install --yes exuberant-ctags
+# try to install deps with homebrew/linuxbrew first
+if which brew; then
+    echo "installing deps with brew ..."
+    brew install ctags
+else
+    echo "installing deps with apt ..."
+    sudo apt update && sudo apt install --yes exuberant-ctags
+fi
+
 sudo python3 -m pip install -U pip jedi black
