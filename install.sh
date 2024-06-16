4b7845b827774a41340c8e9bb21f66f7f8f8186f
guoqiao <guoqiao@gmail.com>
2024-06-16 20:41:23 +1200

    install.sh: link gitconfig

--- install.sh
+++ install.sh
@@ -9,9 +9,10 @@ then
     git clone https://github.com/guoqiao/vimrc.git $DIR
     cd $DIR
     make all
-    if [ ! -f ~/.gitconfig ]
+    cd ~
+    if [ ! -f .gitconfig ]
     then
         echo "create symlink for ~/.gitconfig, please change email and user info"
-        ln -s gitconfig ~/.gitconfig
+        ln -s .vim/gitconfig .gitconfig
     fi
 fi
